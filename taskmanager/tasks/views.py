8e00c22ed7fb9bc53414de60a4fc0785dcda34fa
YunadoofX <yuvak.chitturi@outlook.com>
2025-09-02 15:11:15 -0500

    added django dependencies and some task-website code

---
+++ taskmanager/tasks/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, "tasks/task_list.html", {"tasks": tasks})
+
+def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        if title:
+            Task.objects.create(title=title)
+        return redirect("task_list")
+    return render(request, "tasks/add_task.html")
+
+def toggle_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect("task_list")
